968eca16db10a670a8d79c67d2799f8640bb8cd5
Ajay Karpur <ajay@somaticlabs.io>
2017-08-31 23:10:36 -0500

    updated Market skeleton

--- environment.py
+++ environment.py
@@ -1,7 +1,20 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
 import ccxt
 
-from gym.core import Env
+class Market(gym.Env):
+	metadata = {'render.modes': ['human']}
 
-class Market(Env):
-	def __init__(self, exchange = ccxt.bittrex()):
+	def __init__(self):
+		pass
+
+	def _step(self, action):
+		pass
+
+	def _reset(self):
+		pass
+
+	def _render(self, mode='human', close=False):
 		pass
