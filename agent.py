fc04dba642a079ed8fded25b5282c40a441aa63c
Ajay Karpur <ajay@somaticlabs.io>
2017-09-01 23:53:11 -0500

    Trader class works

--- agent.py
+++ agent.py
@@ -1,18 +1,18 @@
 import tensorflow as tf
 import tensorflow.contrib.keras as keras
 
-class DRQN():
+class DRQN(object):
 	"""
 	Deep recurrent Q-network.
 	"""
-	def __init__(self, exchange, ticker):
+	def __init__(self):
 		pass
 
 	def set_model(self, model):
 		pass
 
-	def train(self, epochs=500, batch_size=32):
+	def train(self, n_epochs=500, batch_size=32):
 		pass
 
-	def play(self, epochs):
+	def play(self, n_epochs):
 		pass
