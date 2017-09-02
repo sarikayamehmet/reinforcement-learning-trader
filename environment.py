2fbb6b95c339c6c9d8f8df65232cdaa037a47ccd
Ajay Karpur <ajay@somaticlabs.io>
2017-09-02 14:26:24 -0500

    comments

--- environment.py
+++ environment.py
@@ -15,9 +15,27 @@ class Market(gym.Env):
 	def __init__(self, exchange, symbol):
 		self.exchange = exchange
 		markets = exchange.load_markets()
-		print(exchange.id, markets)
+		#print(exchange.id, markets)
+		self.action_space = None
+		self.observation_space = None
 
 	def _step(self, action):
+		"""
+		Returns four values:
+		- observation (object): an environment-specific object representing your
+								observation of the environment. For example, the
+								order book for a trading pair.
+		- reward (float): amount of reward achieved by the previous action.
+		- done (boolean): whether it's time to reset the environment again. When
+						  done is True, the episode has terminated (e.g. a
+						  specified threshold of money lost has been reached.)
+		- info (dict): diagnostic information useful for debugging. It can
+					   sometimes be useful for learning (for example, it might
+					   contain the raw probabilities behind the environment's
+					   last state change). However, official evaluations of your
+					   agent are not allowed to use this for learning. (This
+					   could be the price history for a trading pair?)
+		"""
 		pass
 
 	def _reset(self):
@@ -30,4 +48,11 @@ class Market(gym.Env):
 		pass
 
 	def _seed(self):
+		pass
+
+class MarketSpace(gym.Space):
+	"""
+	https://gym.openai.com/docs
+	"""
+	def __init__(self):
 		pass
