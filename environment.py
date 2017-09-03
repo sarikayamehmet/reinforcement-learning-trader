21aa040f34ac5159aeada853b35dba8b0b704f83
Ajay Karpur <ajay@somaticlabs.io>
2017-09-03 11:13:44 -0500

    render modes

--- environment.py
+++ environment.py
@@ -8,38 +8,41 @@ import ccxt
 class Market(gym.Env):
 	"""
 	The main OpenAI Gym class. It encapsulates an environment with
-    arbitrary behind-the-scenes dynamics. An environment can be
-    partially or fully observed.
-    The main API methods that users of this class need to know are:
-        step
-        reset
-        render
-        close
-        seed
-    When implementing an environment, override the following methods
-    in your subclass:
-        _step
-        _reset
-        _render
-        _close
-        _seed
-    And set the following attributes:
-        action_space: The Space object corresponding to valid actions
-        observation_space: The Space object corresponding to valid observations
-        reward_range: A tuple corresponding to the min and max possible rewards
-    Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
-    The methods are accessed publicly as "step", "reset", etc.. The
-    non-underscored versions are wrapper methods to which we may add
-    functionality over time.
-    """
+	arbitrary behind-the-scenes dynamics. An environment can be
+	partially or fully observed.
+	The main API methods that users of this class need to know are:
+		step
+		reset
+		render
+		close
+		seed
+	When implementing an environment, override the following methods
+	in your subclass:
+		_step
+		_reset
+		_render
+		_close
+		_seed
+	And set the following attributes:
+		action_space: The Space object corresponding to valid actions
+		observation_space: The Space object corresponding to valid observations
+		reward_range: A tuple corresponding to the min and max possible rewards
+	Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
+	The methods are accessed publicly as "step", "reset", etc.. The
+	non-underscored versions are wrapper methods to which we may add
+	functionality over time.
+	"""
 
-	metadata = {'render.modes': ['human']}
-	reward_range = (-np.inf, np.inf) # Do I want to 2x or 10x penalize losses?
+	metadata = {
+		'render.modes': ['human', 'ansi']
+	}
 
 	action_space = None
 	observation_space = None
+	reward_range = (-np.inf, np.inf) # Do I want to 2x or 10x penalize losses?
 
 	def __init__(self, exchange, symbol):
+		self.seed()
 		self.exchange = exchange
 		markets = exchange.load_markets()
 		#print(exchange.id, markets)
@@ -47,85 +50,90 @@ class Market(gym.Env):
 	def _step(self, action):
 		"""
 		Run one timestep of the environment's dynamics. When end of
-        episode is reached, you are responsible for calling `reset()`
-        to reset this environment's state.
-        Accepts an action and returns a tuple (observation, reward, done, info).
-        Args:
-            action (object): an action provided by the environment
-        Returns:
-            observation (object): agent's observation of the current environment
-            reward (float) : amount of reward returned after previous action
-            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
-            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
-        """
+		episode is reached, you are responsible for calling `reset()`
+		to reset this environment's state.
+		Accepts an action and returns a tuple (observation, reward, done, info).
+		Args:
+			action (object): an action provided by the environment
+		Returns:
+			observation (object): agent's observation of the current environment
+			reward (float) : amount of reward returned after previous action
+			done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+			info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+		"""
 		pass
 
 	def _reset(self):
 		"""
 		Resets the state of the environment and returns an initial observation.
-        Returns: observation (object): the initial observation of the
-            space.
-        """
+		Returns: observation (object): the initial observation of the
+			space.
+		"""
 		pass
 
 	def _render(self, mode='human', close=False):
 		"""
 		Renders the environment.
-        The set of supported modes varies per environment. (And some
-        environments do not support rendering at all.) By convention,
-        if mode is:
-        - human: render to the current display or terminal and
-          return nothing. Usually for human consumption.
-        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
-          representing RGB values for an x-by-y pixel image, suitable
-          for turning into a video.
-        - ansi: Return a string (str) or StringIO.StringIO containing a
-          terminal-style text representation. The text can include newlines
-          and ANSI escape sequences (e.g. for colors).
-        Note:
-            Make sure that your class's metadata 'render.modes' key includes
-              the list of supported modes. It's recommended to call super()
-              in implementations to use the functionality of this method.
-        Args:
-            mode (str): the mode to render with
-            close (bool): close all open renderings
-        Example:
-        class MyEnv(Env):
-            metadata = {'render.modes': ['human', 'rgb_array']}
-            def render(self, mode='human'):
-                if mode == 'rgb_array':
-                    return np.array(...) # return RGB frame suitable for video
-                elif mode is 'human':
-                    ... # pop up a window and render
-                else:
-                    super(MyEnv, self).render(mode=mode) # just raise an exception
-        """
-		pass
+		The set of supported modes varies per environment. (And some
+		environments do not support rendering at all.) By convention,
+		if mode is:
+		- human: render to the current display or terminal and
+		  return nothing. Usually for human consumption.
+		- rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+		  representing RGB values for an x-by-y pixel image, suitable
+		  for turning into a video.
+		- ansi: Return a string (str) or StringIO.StringIO containing a
+		  terminal-style text representation. The text can include newlines
+		  and ANSI escape sequences (e.g. for colors).
+		Note:
+			Make sure that your class's metadata 'render.modes' key includes
+			  the list of supported modes. It's recommended to call super()
+			  in implementations to use the functionality of this method.
+		Args:
+			mode (str): the mode to render with
+			close (bool): close all open renderings
+		Example:
+		class MyEnv(Env):
+			metadata = {'render.modes': ['human', 'rgb_array']}
+			def render(self, mode='human'):
+				if mode == 'rgb_array':
+					return np.array(...) # return RGB frame suitable for video
+				elif mode is 'human':
+					... # pop up a window and render
+				else:
+					super(MyEnv, self).render(mode=mode) # just raise an exception
+		"""
+		if mode == 'ansi':
+			raise NotImplementedError
+		elif mode == 'human':
+			raise NotImplementedError
+		else:
+			super(Market, self).render(mode=mode) # raise an exception
 
 	def _close(self):
 		"""
 		Override _close in your subclass to perform any necessary cleanup.
-        Environments will automatically close() themselves when
-        garbage collected or when the program exits.
-        """
+		Environments will automatically close() themselves when
+		garbage collected or when the program exits.
+		"""
 		pass
 
 	def _seed(self, seed=None):
 		"""
 		Sets the seed for this env's random number generator(s).
-        Note:
-            Some environments use multiple pseudorandom number generators.
-            We want to capture all such seeds used in order to ensure that
-            there aren't accidental correlations between multiple generators.
-        Returns:
-            list<bigint>: Returns the list of seeds used in this env's random
-              number generators. The first value in the list should be the
-              "main" seed, or the value which a reproducer should pass to
-              'seed'. Often, the main seed equals the provided 'seed', but
-              this won't be true if seed=None, for example.
-        """
+		Note:
+			Some environments use multiple pseudorandom number generators.
+			We want to capture all such seeds used in order to ensure that
+			there aren't accidental correlations between multiple generators.
+		Returns:
+			list<bigint>: Returns the list of seeds used in this env's random
+			  number generators. The first value in the list should be the
+			  "main" seed, or the value which a reproducer should pass to
+			  'seed'. Often, the main seed equals the provided 'seed', but
+			  this won't be true if seed=None, for example.
+		"""
 		self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+		return [seed]
 
 class MarketSpace(gym.Space):
 	"""
