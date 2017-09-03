5967a2ac26fe143b54804ba670a048b5351b8959
Ajay Karpur <ajay@somaticlabs.io>
2017-09-02 23:01:54 -0500

    comments

--- environment.py
+++ environment.py
@@ -7,9 +7,31 @@ import ccxt
 
 class Market(gym.Env):
 	"""
-	How to subclass Env:
-	https://github.com/openai/gym/blob/master/gym/core.py
-	"""
+	The main OpenAI Gym class. It encapsulates an environment with
+    arbitrary behind-the-scenes dynamics. An environment can be
+    partially or fully observed.
+    The main API methods that users of this class need to know are:
+        step
+        reset
+        render
+        close
+        seed
+    When implementing an environment, override the following methods
+    in your subclass:
+        _step
+        _reset
+        _render
+        _close
+        _seed
+    And set the following attributes:
+        action_space: The Space object corresponding to valid actions
+        observation_space: The Space object corresponding to valid observations
+        reward_range: A tuple corresponding to the min and max possible rewards
+    Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
+    The methods are accessed publicly as "step", "reset", etc.. The
+    non-underscored versions are wrapper methods to which we may add
+    functionality over time.
+    """
 
 	metadata = {'render.modes': ['human']}
 	reward_range = (-np.inf, np.inf) # Do I want to 2x or 10x penalize losses?
